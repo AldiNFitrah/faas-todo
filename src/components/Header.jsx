6fdfb4a3abdd6bd1811db0898bb08ded96bc40f1
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-11-21 00:16:14 +0700

    feat(header): create header and configure tailwindcss

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header className="bg-indigo flex flex-col align-middle justify-center">
+      <h1 className="text-7xl text-white mt-16 text-center font-bold">
+        Everyone's ToDos
+      </h1>
+      <h2 className="text-white mt-6 mb-16 text-center text-4xl">
+        Why bother doing your todos when someone else can have it done for you?
+      </h2>
+    </header>
+  );
+}
+
+export default Header;
