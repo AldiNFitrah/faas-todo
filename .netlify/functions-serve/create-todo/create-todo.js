8bbf347de771fb92a37a119eada9704255c73cc0
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-11-21 22:14:46 +0700

    feat(functions): create create-todo function

---
+++ .netlify/functions-serve/create-todo/create-todo.js
@@ -0,0 +1,1 @@
+module.exports = require('./src/src/functions/create-todo.js')
---
+++ src/functions/create-todo.js
@@ -0,0 +1,32 @@
+import supabase from "../database/supabase";
+
+exports.handler = async function (event, context, callback) {
+  const { body } = event;
+  const { title } = JSON.parse(body);
+
+  const { data, status, error } = await supabase.from("todos").insert({
+    title,
+  });
+
+  let response;
+  if (!String(status).startsWith('2') || error) {
+    response = {
+      statuCode: 500,
+      body: JSON.stringify({
+        error: "Something went wrong, please try again later",
+      }),
+    };
+  } else {
+    response = {
+      statusCode: status,
+      body: JSON.stringify({
+        data,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  callback(null, response);
+};
